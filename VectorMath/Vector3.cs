6822447cf5bce9915b68c9dcc982c4e26f947df7
Samuel Brunner <sbrunner5124@ksu.edu>
2019-12-09 14:14:47 -0600

    done

--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -17,12 +17,71 @@ namespace VectorMath
             Z = z;
         }
 
+        public override string ToString()
+        {
+            return $"<{X}, {Y}, {Z}>";
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+
         // Vector Addition
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public void Add(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
 
         // Vector Subtraction 
 
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
         // Vector Scaling 
+        public static Vector3 operator *(double s, Vector3 v)
+        {
+            return new Vector3(v.X * s, v.Y * s, v.Z *s);
+        }
+
+        public static Vector3 operator *( Vector3 v, double s)
+        {
+            return new Vector3(v.X * s, v.Y * s, v.Z * s);
+        }
 
         // Vector Comparison
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+        }
+
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public static bool operator true(Vector3 a)
+        {
+            return Vector3.Zero != a;
+        }
+
+        public static bool operator false(Vector3 a)
+        {
+            return Vector3.Zero == a;
+        }
+
+
     }
 }
